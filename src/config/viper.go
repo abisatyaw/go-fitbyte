421b2d52d14a7600705d4d450d484d0febf429ad
abisatyaw <abisatya90@gmail.com>
2025-09-07 13:08:58 +0000

    test: add env to docker-compose

--- src/config/viper.go
+++ src/config/viper.go
@@ -1,8 +1,6 @@
 package config
 
 import (
-	"fmt"
-
 	"github.com/spf13/viper"
 )
 
@@ -11,18 +9,7 @@ func NewViper() *viper.Viper {
 	config := viper.New()
 
 	// Set config to read from .env file
-	config.SetConfigName(".env")
-	config.SetConfigType("env")
-	config.AddConfigPath("./")
-	
-	// Enable reading from environment variables
 	config.AutomaticEnv()
-
-	err := config.ReadInConfig()
-
-	if err != nil {
-		panic(fmt.Errorf("Fatal error config file: %w \n", err))
-	}
 
 	return config
 }
